7be5c6d1e8249c7742bc7acafc5d311336f23360
Andrew Wason <rectalogic@rectalogic.com>
2019-12-12 14:48:41 -0500

    Improve argument validation.

--- lispy/core.py
+++ lispy/core.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import time
+import operator
 from typing import List, Union, Dict, NoReturn, cast
 
 from . import reader
@@ -393,61 +394,83 @@ def swap(args: List[MalExpression]) -> MalExpression:
     return atom.native()
 
 
+def require_args(args: List[MalExpression], count: int) -> List[MalExpression]:
+    if len(args) != count:
+        raise MalSyntaxException("not enough arguments")
+    return args
+
+
 ns = {
-    "+": MalFunctionCompiled(lambda args: MalInt(args[0].native() + args[1].native())),
-    "-": MalFunctionCompiled(lambda args: MalInt(args[0].native() - args[1].native())),
-    "*": MalFunctionCompiled(lambda args: MalInt(args[0].native() * args[1].native())),
+    "+": MalFunctionCompiled(
+        lambda args: MalInt(operator.add(*[a.native() for a in require_args(args, 2)]))
+    ),
+    "-": MalFunctionCompiled(
+        lambda args: MalInt(operator.sub(*[a.native() for a in require_args(args, 2)]))
+    ),
+    "*": MalFunctionCompiled(
+        lambda args: MalInt(operator.mul(*[a.native() for a in require_args(args, 2)]))
+    ),
     "/": MalFunctionCompiled(
-        lambda args: MalInt(int(args[0].native() / args[1].native()))
+        lambda args: MalInt(
+            operator.floordiv(*[a.native() for a in require_args(args, 2)])
+        )
     ),
     "prn": MalFunctionCompiled(lambda args: prn(args)),
     "pr-str": MalFunctionCompiled(lambda args: pr_str(args)),
     "println": MalFunctionCompiled(lambda args: println(args)),
     "list": MalFunctionCompiled(lambda args: MalList(args)),
-    "list?": MalFunctionCompiled(lambda args: list_q(args[0])),
-    "empty?": MalFunctionCompiled(lambda args: empty_q(args[0])),
-    "count": MalFunctionCompiled(lambda args: count(args[0])),
-    "=": MalFunctionCompiled(lambda args: equal(args[0], args[1])),
-    "<": MalFunctionCompiled(lambda args: less(args[0], args[1])),
-    "<=": MalFunctionCompiled(lambda args: less_equal(args[0], args[1])),
-    ">": MalFunctionCompiled(lambda args: less(args[1], args[0])),
-    ">=": MalFunctionCompiled(lambda args: less_equal(args[1], args[0])),
-    "read-string": MalFunctionCompiled(lambda args: read_string(args[0])),
-    "slurp": MalFunctionCompiled(lambda args: slurp(args[0])),
+    "list?": MalFunctionCompiled(lambda args: list_q(require_args(args, 1)[0])),
+    "empty?": MalFunctionCompiled(lambda args: empty_q(require_args(args, 1)[0])),
+    "count": MalFunctionCompiled(lambda args: count(require_args(args, 1)[0])),
+    "=": MalFunctionCompiled(lambda args: equal(*require_args(args, 2))),
+    "<": MalFunctionCompiled(lambda args: less(*require_args(args, 2))),
+    "<=": MalFunctionCompiled(lambda args: less_equal(*require_args(args, 2))),
+    ">": MalFunctionCompiled(lambda args: less(*list(reversed(require_args(args, 2))))),
+    ">=": MalFunctionCompiled(
+        lambda args: less_equal(*list(reversed(require_args(args, 2))))
+    ),
+    "read-string": MalFunctionCompiled(
+        lambda args: read_string(require_args(args, 1)[0])
+    ),
+    "slurp": MalFunctionCompiled(lambda args: slurp(require_args(args, 1)[0])),
     "str": MalFunctionCompiled(lambda args: core_str(args)),
-    "atom": MalFunctionCompiled(lambda args: MalAtom(args[0])),
-    "atom?": MalFunctionCompiled(lambda args: MalBoolean(isinstance(args[0], MalAtom))),
-    "deref": MalFunctionCompiled(lambda args: deref_q(args[0])),
-    "reset!": MalFunctionCompiled(lambda args: reset(args[0], args[1])),
-    "cons": MalFunctionCompiled(lambda args: cons(args[0], args[1])),
+    "atom": MalFunctionCompiled(lambda args: MalAtom(require_args(args, 1)[0])),
+    "atom?": MalFunctionCompiled(
+        lambda args: MalBoolean(isinstance(require_args(args, 1)[0], MalAtom))
+    ),
+    "deref": MalFunctionCompiled(lambda args: deref_q(require_args(args, 1)[0])),
+    "reset!": MalFunctionCompiled(lambda args: reset(*require_args(args, 2))),
+    "cons": MalFunctionCompiled(lambda args: cons(*require_args(args, 2))),
     "concat": MalFunctionCompiled(concat),
-    "not": MalFunctionCompiled(lambda args: not_(args[0])),
-    "nth": MalFunctionCompiled(lambda args: nth(args[0], args[1])),
+    "not": MalFunctionCompiled(lambda args: not_(require_args(args, 1)[0])),
+    "nth": MalFunctionCompiled(lambda args: nth(*require_args(args, 2))),
     "apply": MalFunctionCompiled(lambda args: apply(args)),
-    "map": MalFunctionCompiled(lambda args: map_(args[0], args[1])),
-    "throw": MalFunctionCompiled(lambda args: throw(args[0])),
-    "nil?": MalFunctionCompiled(lambda args: nil_q(args[0])),
-    "true?": MalFunctionCompiled(lambda args: true_q(args[0])),
-    "false?": MalFunctionCompiled(lambda args: false_q(args[0])),
-    "symbol": MalFunctionCompiled(lambda args: symbol(args[0])),
-    "symbol?": MalFunctionCompiled(lambda args: symbol_q(args[0])),
-    "readline": MalFunctionCompiled(lambda args: readline(args[0])),
+    "map": MalFunctionCompiled(lambda args: map_(*require_args(args, 2))),
+    "throw": MalFunctionCompiled(lambda args: throw(require_args(args, 1)[0])),
+    "nil?": MalFunctionCompiled(lambda args: nil_q(require_args(args, 1)[0])),
+    "true?": MalFunctionCompiled(lambda args: true_q(require_args(args, 1)[0])),
+    "false?": MalFunctionCompiled(lambda args: false_q(require_args(args, 1)[0])),
+    "symbol": MalFunctionCompiled(lambda args: symbol(require_args(args, 1)[0])),
+    "symbol?": MalFunctionCompiled(lambda args: symbol_q(require_args(args, 1)[0])),
+    "readline": MalFunctionCompiled(lambda args: readline(require_args(args, 1)[0])),
     "time-ms": MalFunctionCompiled(lambda args: MalInt(int(time.time() * 1000))),
     "meta": MalFunctionCompiled(lambda args: not_implemented("meta")),
     "with-meta": MalFunctionCompiled(lambda args: not_implemented("with-meta")),
     "fn?": MalFunctionCompiled(lambda args: not_implemented("fn?")),
     "string?": MalFunctionCompiled(lambda args: not_implemented("string?")),
     "number?": MalFunctionCompiled(lambda args: not_implemented("number?")),
-    "seq": MalFunctionCompiled(lambda args: seq(args[0])),
+    "seq": MalFunctionCompiled(lambda args: seq(require_args(args, 1)[0])),
     "conj": MalFunctionCompiled(lambda args: not_implemented("conj")),
-    "get": MalFunctionCompiled(lambda args: get(args[0], args[1])),
+    "get": MalFunctionCompiled(lambda args: get(*require_args(args, 2))),
     "first": MalFunctionCompiled(lambda args: first(args)),
     "rest": MalFunctionCompiled(lambda args: rest(args)),
-    "keyword?": MalFunctionCompiled(lambda args: keyword_q(args[0])),
-    "keyword": MalFunctionCompiled(lambda args: keyword(args[0])),
-    "vector?": MalFunctionCompiled(lambda args: vector_q(args[0])),
-    "map?": MalFunctionCompiled(lambda args: map_q(args[0])),
-    "sequential?": MalFunctionCompiled(lambda args: sequential_q(args[0])),
+    "keyword?": MalFunctionCompiled(lambda args: keyword_q(require_args(args, 1)[0])),
+    "keyword": MalFunctionCompiled(lambda args: keyword(require_args(args, 1)[0])),
+    "vector?": MalFunctionCompiled(lambda args: vector_q(require_args(args, 1)[0])),
+    "map?": MalFunctionCompiled(lambda args: map_q(require_args(args, 1)[0])),
+    "sequential?": MalFunctionCompiled(
+        lambda args: sequential_q(require_args(args, 1)[0])
+    ),
     "vector": MalFunctionCompiled(lambda args: vector(args)),
     "hash-map": MalFunctionCompiled(lambda args: hash_map(args)),
     "assoc": MalFunctionCompiled(lambda args: assoc(args)),
